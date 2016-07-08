776f4c85fa20c3233b9d341a9a347eed3edbf010
Paulo Lourenço <paulolourenco13@gmail.com>
2016-07-08 12:17:47 -0300

    Correção de bugs na interface

--- app/src/main/java/com/example/pauloloureno/buipost/Registrar.java
+++ app/src/main/java/com/example/pauloloureno/buipost/Registrar.java
@@ -18,40 +18,11 @@ public class Registrar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrar);
 
-        final Button botaoFechar = (Button) findViewById(R.id.botao_fechar);
-        final EditText email = (EditText) findViewById(R.id.textEmailTelaPrincipal);
-        final EditText senha = (EditText) findViewById(R.id.textSenha_Registrar);
-        final EditText repeat = (EditText) findViewById(R.id.textSenha_Registrar_Repeat);
-
-       /* botaoFechar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });*/
+        EditText email = (EditText) findViewById(R.id.textEmailTelaPrincipal);
+        EditText senha = (EditText) findViewById(R.id.textSenha_Registrar);
+        EditText repeat = (EditText) findViewById(R.id.textSenha_Registrar_Repeat);
 
 
-
-        email.setOnClickListener(new Button.OnClickListener()
-        {
-            public void onClick(View view) {
-                    email.setText("");
-            }
-        });
-        senha.setOnClickListener(new Button.OnClickListener()
-        {
-            public void onClick(View view) {
-                senha.setInputType(1);
-                senha.setText("");
-            }
-        });
-
-        repeat.setOnClickListener(new Button.OnClickListener()
-        {
-            public void onClick(View view) {
-                repeat.setText("");
-            }
-        });
 
 
     }
