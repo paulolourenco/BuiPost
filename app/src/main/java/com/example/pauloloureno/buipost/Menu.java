3f5c9b65183d0a17e0acf186ac44e55b48a4ba70
Paulo Lourenço <paulolourenco13@gmail.com>
2016-07-11 17:35:37 -0300

    Revert "Incluindo tela"
    This reverts commit 74ba158e80b8e068896a459b1692910f1c797aa3.

--- app/src/main/java/com/example/pauloloureno/buipost/Menu.java
+++ app/src/main/java/com/example/pauloloureno/buipost/Menu.java
@@ -8,11 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 
 /**
@@ -21,66 +16,32 @@ import android.support.v7.widget.Toolbar;
 public class Menu extends AppCompatActivity {
 
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.menu);
-//
-//        Button proximidades = (Button) findViewById(R.id.botao_proximidades);
-//
-//        /*proximidades.setOnClickListener(new Button.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent =  new Intent(Menu.this, Proximidades.class);
-//                startActivity ( intent );
-//            }
-//        });*/
-//
-//
-//    }
-
-//    public void proximidades (View view){
-//        Intent intent =  new Intent(Menu.this, Proximidades.class);
-//        startActivity ( intent );
-//    }
-//    public void postAmigos (View view){
-//        Intent intent =  new Intent(Menu.this, SplashActivity.class);
-//        startActivity ( intent );
-//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.menu);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        Button proximidades = (Button) findViewById(R.id.botao_proximidades);
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter
-                (getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
+        /*proximidades.setOnClickListener(new Button.OnClickListener(){
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
+            public void onClick(View view) {
+                Intent intent =  new Intent(Menu.this, Proximidades.class);
+                startActivity ( intent );
             }
+        });*/
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
 
-            }
-        });
+    }
 
-}}
+    public void proximidades (View view){
+        Intent intent =  new Intent(Menu.this, Proximidades.class);
+        startActivity ( intent );
+    }
+    public void postAmigos (View view){
+        Intent intent =  new Intent(Menu.this, SplashActivity.class);
+        startActivity ( intent );
+    }
+
+}
