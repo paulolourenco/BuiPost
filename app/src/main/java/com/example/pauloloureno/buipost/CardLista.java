2d84a186e83cd51375843de015603fbd08f330fe
amandacvl <amandalobo2005@gmail.com>
2016-07-20 12:20:30 -0300

    Mudança na MainActvity

---
+++ app/src/main/java/com/example/pauloloureno/buipost/CardLista.java
@@ -0,0 +1,20 @@
+package com.example.pauloloureno.buipost;
+
+/**
+ * Created by Paf on 20/07/2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CardLista extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.itemcard, null);
+    }
+}
